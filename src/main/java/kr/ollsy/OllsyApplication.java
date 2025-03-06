f96cd25dd115f56d4c989ea6b7aaaf429b0e6f44
sangjin <insangjin6439@gmail.com>
2025-03-06 16:49:00 +0900

    first commit

---
+++ src/main/java/kr/ollsy/OllsyApplication.java
@@ -0,0 +1,13 @@
+package kr.ollsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OllsyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OllsyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ollsy/OllsyApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ollsy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OllsyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
