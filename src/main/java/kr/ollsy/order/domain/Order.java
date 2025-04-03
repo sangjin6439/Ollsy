20c688fd5e526a5f4f05d04dfec364fab80168a3
sangjin <insangjin6439@gmail.com>
2025-04-03 18:24:34 +0900

    [feat/#21] Order엔티티 생성

--- src/main/java/kr/ollsy/order/domain/Order.java
+++ src/main/java/kr/ollsy/order/domain/Order.java
@@ -1,4 +1,39 @@
 package kr.ollsy.order.domain;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import kr.ollsy.order_item.domain.OrderItem;
+import kr.ollsy.user.domain.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "orders")
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_Id")
+    private User user;
+
+    @OneToMany(mappedBy = "order",cascade = CascadeType.ALL) //OrderItem과 생명주기가 같음
+    private List<OrderItem> orderItems;
+
 }
