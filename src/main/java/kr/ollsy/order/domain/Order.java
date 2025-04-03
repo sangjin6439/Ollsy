142a37af2c01cf217836210ac222106f80d2418e
sangjin <insangjin6439@gmail.com>
2025-04-03 18:38:40 +0900

    [feat/#21] -OrderItem엔티티 생성, Order테이블 totalPrice필드 추가 및 DateEntity 상속

--- src/main/java/kr/ollsy/order/domain/Order.java
+++ src/main/java/kr/ollsy/order/domain/Order.java
@@ -3,6 +3,7 @@ package kr.ollsy.order.domain;
 import java.util.List;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -12,28 +13,33 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import kr.ollsy.global.entity.DateEntity;
 import kr.ollsy.order_item.domain.OrderItem;
 import kr.ollsy.user.domain.User;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "orders")
-public class Order {
+public class Order extends DateEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "orders_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="user_Id")
+    @JoinColumn(name="users_Id")
     private User user;
 
     @OneToMany(mappedBy = "order",cascade = CascadeType.ALL) //OrderItem과 생명주기가 같음
     private List<OrderItem> orderItems;
+
+    private int totalPrice;
 
 }
--- src/main/java/kr/ollsy/order_item/domain/OrderItem.java
+++ src/main/java/kr/ollsy/order_item/domain/OrderItem.java
@@ -1,4 +1,41 @@
 package kr.ollsy.order_item.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import kr.ollsy.item.domain.Item;
+import kr.ollsy.order.domain.Order;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "order_items")
 public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_items_id")
+    Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "orders_id")
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "items_id")
+    private Item item;
+
+    private int quantity;
+
+    private int orderPrice;
 }
