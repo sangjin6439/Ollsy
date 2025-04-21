929973fa49be7930c6c2eba8de59d2b59f90dc5b
sangjin <insangjin6439@gmail.com>
2025-04-21 14:15:26 +0900

    [feat/#5] -Swagger 라이브러리 추가 및 swagger와 저번 맞추기 위해 springboot 버전 3.2.3으로 변경

---
+++ src/main/java/kr/ollsy/global/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package kr.ollsy.global.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Ollsy API")
+                        .version("v1")
+                        .description("Ollsy API 명세서"));
+
+    }
+}
