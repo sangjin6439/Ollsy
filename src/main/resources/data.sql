2d13ed2b126e2f432b2b274885e2ae1b6308db50
sangjin <insangjin6439@gmail.com>
2025-05-12 18:07:27 +0900

    local과 prod.yml 설정 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,9 +5,9 @@ INSERT INTO categories(category_id,name,parent_id,depth) value (4,'셔츠',1,1);
 INSERT INTO categories(category_id,name,parent_id,depth) value (5,'져지',1,1);
 INSERT INTO categories(category_id,name,parent_id,depth) value (6,'반바지',2,1);
 
-INSERT INTO item_images(item_image_id,url) value (1, 'https://ollsy.s3.ap-northeast-2.amazonaws.com/item-imagesea90b7ef-ab50-4753-a0b6-b9fd9412cb57.png');
-INSERT INTO item_images(item_image_id,url) value (2, 'https://ollsy.s3.ap-northeast-2.amazonaws.com/item-images51ba5e39-dc23-4b86-b60d-414310386997.png');
-INSERT INTO item_images(item_image_id,url) value (3, 'https://ollsy.s3.ap-northeast-2.amazonaws.com/item-images58316170-7d63-4bfc-ac0f-7a3553e758a2.png');
+INSERT INTO item_images(item_image_id,url) value (1, 'https://ollsy.s3.ap-northeast-2.amazonaws.com/item-images57980b09-c8f0-4ecf-8291-37023925ecdd.png');
+INSERT INTO item_images(item_image_id,url) value (2, 'https://ollsy.s3.ap-northeast-2.amazonaws.com/item-images8dc89e2e-4517-4258-9ed5-c8cfbc261304.png');
+INSERT INTO item_images(item_image_id,url) value (3, 'https://ollsy.s3.ap-northeast-2.amazonaws.com/item-images4c2c523e-525c-425d-899b-b376a0d99e6a.png');
 INSERT INTO item_images(item_image_id,url) value (4, 'https://ollsy.s3.ap-northeast-2.amazonaws.com/item-images5b0480b8-e737-4d43-aea5-5efb8e5f42e9.png');
 
 INSERT INTO items(item_id,category_id,name,price,stock,description) value (1, 3,'라운드 니트','50000','100','부드러운 니트');
@@ -21,3 +21,4 @@ UPDATE item_images SET item_item_id = 2 WHERE item_image_id = 2;
 UPDATE item_images SET item_item_id = 3 WHERE item_image_id = 3;
 UPDATE item_images SET item_item_id = 4 WHERE item_image_id = 4;
 UPDATE items SET create_at = NOW(), update_at = NOW();
+
